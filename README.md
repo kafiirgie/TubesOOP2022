be8c1030f03d521a14aa73c4a5318ae15275fc7d
kafiirgie <kafiirgie20@gmail.com>
2022-03-22 21:17:10 +0700

    upload template

---
+++ README.md
@@ -0,0 +1,22 @@
+# IF2212 Project Template
+
+Here is an example of project using gradle as the build tools.
+Try running these commands:
+
+On linux:
+```bash
+./gradlew run
+```
+
+On windows:
+```cmd
+gradlew.bat run
+```
+
+Notice that it will run the `Main.java` inside the `com.monstersaku` package.
+In the command line you can see the data that is being read by `CSVReader.java`.
+
+What happen is when you use `./gradlew run`, it will start the main function in your app.
+For this app, the main function lives in `Main.java`.
+
+You can explore more about gradle [here](https://guides.gradle.org/creating-new-gradle-builds/)
---
+++ src/main/java/com/monstersaku/Main.java
@@ -0,0 +1,38 @@
+package com.monstersaku;
+
+import com.monstersaku.util.CSVReader;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main {
+    private static final List<String> CSV_FILE_PATHS = Collections.unmodifiableList(Arrays.asList(
+            "configs/monsterpool.csv",
+            "configs/movepool.csv",
+            "configs/element-type-effectivity-chart.csv"));
+
+    public static void main(String[] args) {
+        for (String fileName : CSV_FILE_PATHS) {
+            try {
+                System.out.printf("Filename: %s\n", fileName);
+                CSVReader reader = new CSVReader(new File(Main.class.getResource(fileName).toURI()), ";");
+                reader.setSkipHeader(true);
+                List<String[]> lines = reader.read();
+                System.out.println("=========== CONTENT START ===========");
+                for (String[] line : lines) {
+                    for (String word : line) {
+                        System.out.printf("%s ", word);
+                    }
+                    System.out.println();
+                }
+                System.out.println("=========== CONTENT END ===========");
+                System.out.println();
+            } catch (Exception e) {
+                // do nothing
+            }
+        }
+    }
+}
